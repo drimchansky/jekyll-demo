07f80bd68fa8dab8cbbb4f3c9e9f889b856de3f7
Nikita Chernov <drimchansky@gmail.com>
2020-04-02 11:55:38 +0300

    Move configs

--- js/components/hamburger.js
+++ js/components/hamburger.js
@@ -1,11 +1,11 @@
-(() => {
-  const hamburger = document.querySelector('.hamburger');
-  const nav = document.querySelector('.nav');
-  const body = document.getElementsByTagName('body')[0];
+;(() => {
+  const hamburger = document.querySelector('.hamburger')
+  const nav = document.querySelector('.nav')
+  const body = document.getElementsByTagName('body')[0]
 
   hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('is-active');
-    nav.classList.toggle('nav_visible');
-    body.classList.toggle('is-open');
-  });
-})();
+    hamburger.classList.toggle('is-active')
+    nav.classList.toggle('nav_visible')
+    body.classList.toggle('is-open')
+  })
+})()
