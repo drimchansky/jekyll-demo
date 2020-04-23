2bd8e2636628a73b8bcd89faa71e7c5bf0a2a9b5
Nikita Chernov <drimchansky@gmail.com>
2020-04-23 19:40:34 +0300

    Update sticky script

--- js/components/sticky.js
+++ js/components/sticky.js
@@ -1,20 +1,13 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {
-  myFunction()
-}
+{
+  const header = document.querySelector('.header')
+  const sticky = header.offsetTop
 
-// Get the header
-const header = document.querySelector('.header')
-
-// Get the offset position of the navbar
-const sticky = header.offsetTop
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add('header_sticky')
-    console.log('sticky')
-  } else {
-    header.classList.remove('header_sticky')
+  window.onscroll = () => {
+    if (window.pageYOffset > sticky) {
+      header.classList.add('header_sticky')
+      console.log('sticky')
+    } else {
+      header.classList.remove('header_sticky')
+    }
   }
 }
