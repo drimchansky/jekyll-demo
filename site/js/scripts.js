47ef6b0f37cfb91c38790c9d7b51f815cc2296e9
Nikita Chernov <drimchansky@gmail.com>
2020-05-15 14:00:03 +0300

    Add quicklink

--- site/js/scripts.js
+++ site/js/scripts.js
@@ -1,3 +1,8 @@
 import './components/hamburger'
 import './components/map'
 import './components/sticky'
+import quicklink from '../../node_modules/quicklink/dist/quicklink'
+
+window.addEventListener('load', () => {
+  quicklink.listen()
+})
