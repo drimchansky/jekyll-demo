a63ee2d7bb7a38f9414e08dcbebdd099dd1e1a6d
Nikita Chernov <drimchansky@gmail.com>
2020-05-16 06:55:55 +0300

    Remove share button

--- site/js/components/popper.js
+++
@@ -1,8 +0,0 @@
-import { createPopper } from '@popperjs/core/lib/popper-lite.js'
-
-const button = document.querySelector('#social-button')
-const tooltip = document.querySelector('#social-tooltip')
-
-createPopper(button, tooltip, {
-  placement: 'right',
-})
